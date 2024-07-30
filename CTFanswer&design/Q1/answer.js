e2d3fef9c78e3b1b05de9c2512243c17ac040543
FrogGuaGua <948780232@qq.com>
2024-07-30 17:14:50 +0930

    123

--- CTFanswer&design/Q1/answer.js
+++ CTFanswer&design/Q1/answer.js
@@ -1,1 +1,20 @@
-ws.send(JSON.stringify({try:"486163656b343072457173"}))
+const WebSocket = require("ws");
+
+
+let ws = new WebSocket.WebSocket("ws://10.0.0.109:5555")
+ws.on("open", () => {
+    console.log("start")
+    ws.send(JSON.stringify({try:"486163656b343072457173"}))
+});
+
+ws.on("message", (data) => {
+    console.log(JSON.parse(data))
+});
+
+ws.on("close", () => {
+    console.log("closed")
+});
+
+ws.on("error", (error) => {
+    console.log("error")
+});
